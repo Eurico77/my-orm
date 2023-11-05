303d648fdca95d50aba528f6147d8adb75fa6057
eurico77 <euricomagalhaes61@gmail.com>
2023-11-05 11:33:13 -0300

    feat: create base entity and create a entity base

--- src/BaseEntity.ts
+++ src/BaseEntity.ts
@@ -1,2 +1,14 @@
-export class Entity { }
+type Columns = {
+  property: string;
+  column: string;
+}
 
+export class Entity {
+  constructor() {
+    console.log(this.columns);
+  }
+  declare schema: string;
+  declare table: string;
+  declare columns: Columns[];
+  [key: string]: any
+}
--- src/Entities.ts
+++ src/Entities.ts
@@ -1,8 +1,15 @@
-import { Entity } from "./BaseEntity";
+import { Entity } from './BaseEntity';
 
+import { entity } from './decorators/entity.decorator';
+import { column } from './decorators/property.decorator';
 
+@entity({ schema: 'public', table: 'book' })
 export class Book extends Entity {
+
+  @column({ name: 'title' })
   title: string;
+
+  @column({ name: 'author' })
   author: string;
 
   constructor(title: string, author: string) {
