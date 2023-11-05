7c6d06eeacc3c40286fb46fe3c692f3297d80cce
eurico77 <euricomagalhaes61@gmail.com>
2023-11-05 11:35:06 -0300

    feat: remove logs

--- src/decorators/property.decorator.ts
+++ src/decorators/property.decorator.ts
@@ -2,8 +2,6 @@ import { Entity } from '../BaseEntity';
 
 export function column(config: { name: string }) {
   return (target: Entity, propertyKey: string) => {
-    console.log('property key:', propertyKey);
-    console.log('config.name:', config.name);
     target.columns = target.columns || [];
     target.columns.push({ property: propertyKey, column: config.name });
   };
