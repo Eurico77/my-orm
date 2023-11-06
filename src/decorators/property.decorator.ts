1d801652a986ffef18346b7d9bd8cee378018f2a
eurico77 <euricomagalhaes61@gmail.com>
2023-11-06 11:02:39 -0300

    refactor: add logs in decorators

--- src/decorators/property.decorator.ts
+++ src/decorators/property.decorator.ts
@@ -2,6 +2,7 @@ import { Entity } from '../entities/BaseEntity';
 
 export function column(config: { name: string }) {
   return (target: Entity, propertyKey: string) => {
+    console.log(`entity: ${target} with properties: ${propertyKey}`);
     target.columns = target.columns || [];
     target.columns.push({ property: propertyKey, column: config.name });
   };
