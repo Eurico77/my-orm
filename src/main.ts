63f079f3ef921cb405433c956dc600db87f39e02
eurico77 <euricomagalhaes61@gmail.com>
2023-11-06 11:02:03 -0300

    feat: create a new entity in orm

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { Book } from './entities/Book';
+import { Car } from './entities/Car';
 import { ORM } from './infra/database/ORM';
 import { PostgresConnection } from './infra/database/PostgresConnection';
 
 async function init() {
   const connection = new PostgresConnection();
   const orm = new ORM(connection);
+
+
   const book = new Book('Clean code', 'Robert Martin');
   await orm.save(book);
+
+
+  const car = new Car('Fiat', 'FastBack');
+  await orm.save(car);
+
+
   await connection.close();
 }
 init();
